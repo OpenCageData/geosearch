511b38c54bfbb71df330b52d9d07d523a4160f59
Arnaud Ferrand <arferrand@gmail.com>
2021-11-22 18:02:48 +0000

    updates results' footer, adds a html test page for the footer

--- packages/geosearch-core/src/OpenCageGeoSearchPlugin.js
+++ packages/geosearch-core/src/OpenCageGeoSearchPlugin.js
@@ -78,7 +78,22 @@ const OpenCageGeoSearchPlugin = (options = {}, events = {}) => {
             // type: (params: { state: AutocompleteState<TItem>, source: AutocompleteSource<TItem>, items: TItem[], createElement: Pragma, Fragment: PragmaFrag }) => VNode | string
             return createElement('div', {
               dangerouslySetInnerHTML: {
-                __html: `<div style="display:flex;flex-direction:column;align-items:flex-end;"><div style="display:flex;align-items:center;justify-content:center;font-size:0.7em;text-decoration:none;color:#009966;"><span>&nbsp;<a href="https://opencagedata.com/geosearch" target="_blank" rel="noreferrer"><img src="https://assets.opencagedata.com/opencage-20x21.png" height="21" width="20" border="0" alt="OpenCage" style="display:inline;" /></a>&nbsp;&nbsp;Made by <a href="https://opencagedata.com/geosearch" target="_blank" rel="noreferrer">OpenCage</a>.&nbsp;&copy;&nbsp;<a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer">OpenStreetMap</a>,&nbsp;<a href="https://opencagedata.com/credits" target="_blank" rel="noreferrer">others</a>.</span></div></div>`,
+                __html: `
+                <div style="display:flex;flex-direction:column;align-items:flex-end;">
+                <div
+                  style="display:flex;align-items:center;justify-content:center;font-family: sans-serif;font-size:0.7em;color:#009966;">
+                  <span><a href="https://opencagedata.com/geosearch" target="_blank" rel="noreferrer"><img
+                        src="https://assets.opencagedata.com/opencage-20x21.png" height="21" width="20" border="0" alt="OpenCage"
+                        style="display:inline;" /></a></span>
+                  <span>&nbsp;&nbsp;Made by <a href="https://opencagedata.com/geosearch" target="_blank" rel="noreferrer"
+                      style="text-decoration:none;color:#009966;">OpenCage</a>.</span>
+                  <span>&nbsp;&copy;&nbsp;<a href="https://www.openstreetmap.org/copyright" target="_blank" rel="noreferrer"
+                      style="text-decoration:none;color:#009966;">OpenStreetMap</a>,</span>
+                  <span>&nbsp;<a href="https://opencagedata.com/credits" target="_blank" rel="noreferrer"
+                      style="text-decoration:none;color:#009966;">others</a>.</span>
+                </div>
+              </div>
+              `,
               },
             });
           },
