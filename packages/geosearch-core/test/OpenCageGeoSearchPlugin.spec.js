bdd3bc92a90e4b0b6785ab5d1f6c252014d7e26d
Arnaud Ferrand <arferrand@gmail.com>
2022-01-24 20:26:00 +0000

    adds some unit tests

--- packages/geosearch-core/test/OpenCageGeoSearchPlugin.spec.js
+++ packages/geosearch-core/test/OpenCageGeoSearchPlugin.spec.js
@@ -18,9 +18,29 @@ describe('geosearch-core:OpenCageGeoSearchPlugin', () => {
     expect(plugin.onReset()).not.to.throw;
     expect(plugin.onSubmit()).not.to.throw;
   });
-  it(`should return an empty result`, async () => {
+  it(`should return an empty result with an empty query`, async () => {
     const plugin = OpenCageGeoSearchPlugin.OpenCageGeoSearchPlugin();
     const result = await plugin.getSources({ query: '' });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(0);
+  });
+  it(`should return an empty result with a query to short`, async () => {
+    const plugin = OpenCageGeoSearchPlugin.OpenCageGeoSearchPlugin();
+    const result = await plugin.getSources({ query: 'aa' });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(0);
+  });
+  it(`should return an empty result with a non string query`, async () => {
+    const plugin = OpenCageGeoSearchPlugin.OpenCageGeoSearchPlugin();
+    const result = await plugin.getSources({ query: 123 });
+    expect(result).to.be.an('array');
+    expect(result.length).to.equal(0);
+  });
+  it('should return an empty result with a fakekey', async () => {
+    const plugin = OpenCageGeoSearchPlugin.OpenCageGeoSearchPlugin({
+      key: 'fakekey',
+    });
+    const result = await plugin.getSources({ query: 'lyon' });
     expect(result).to.be.an('array');
     expect(result.length).to.equal(0);
   });
